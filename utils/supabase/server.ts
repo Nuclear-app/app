cba78cdf5575c8910fcaa5236e7025c552dc9138
Ojas Srivastava <osrivastava@nakisa.com>
2025-05-05 15:47:24 -0400

    There's a server.ts waiting in the sky

--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -7,8 +7,8 @@ export async function createClient() {
   // Create a server's supabase client with newly configured cookie,
   // which could be used to maintain user's session
   return createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    process.env.SUPABASE_URL!,
+    process.env.SUPABASE_ANON_KEY!,
     {
       cookies: {
         getAll() {
@@ -28,4 +28,17 @@ export async function createClient() {
       },
     }
   )
+}
+
+export async function getUser() {
+  const {auth} = await createClient();
+
+  const userObject = await auth.getUser();
+
+  if(userObject.error) {
+    console.log("Error getting user", userObject.error);
+    return null;
+  }
+
+  return userObject.data.user;
 }
