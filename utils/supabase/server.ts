c5e7ebcc670f5f65f23bf49cd0fd025db1abeb8e
Ojas Srivastava <osrivastava@nakisa.com>
2025-05-12 11:41:13 -0400

    Final init

--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -1,44 +1,29 @@
-import { createServerClient } from '@supabase/ssr'
-import { cookies } from 'next/headers'
+import { createServerClient } from "@supabase/ssr";
+import { cookies } from "next/headers";
 
-export async function createClient() {
-  const cookieStore = await cookies()
+export const createClient = async () => {
+  const cookieStore = await cookies();
 
-  // Create a server's supabase client with newly configured cookie,
-  // which could be used to maintain user's session
   return createServerClient(
-    process.env.SUPABASE_URL!,
-    process.env.SUPABASE_ANON_KEY!,
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
         getAll() {
-          return cookieStore.getAll()
+          return cookieStore.getAll();
         },
         setAll(cookiesToSet) {
           try {
-            cookiesToSet.forEach(({ name, value, options }) =>
-              cookieStore.set(name, value, options)
-            )
-          } catch {
-            // The `setAll` method was called from a Server Component.
+            cookiesToSet.forEach(({ name, value, options }) => {
+              cookieStore.set(name, value, options);
+            });
+          } catch (error) {
+            // The `set` method was called from a Server Component.
             // This can be ignored if you have middleware refreshing
             // user sessions.
           }
         },
       },
-    }
-  )
-}
-
-export async function getUser() {
-  const {auth} = await createClient();
-
-  const userObject = await auth.getUser();
-
-  if(userObject.error) {
-    console.log("Error getting user", userObject.error);
-    return null;
-  }
-
-  return userObject.data.user;
-}
+    },
+  );
+};
