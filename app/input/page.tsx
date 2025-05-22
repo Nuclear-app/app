92abc0383342263f3ba575751ea519f598c3ba31
Karman <shubhkarmansingh27sk@gmail.com>
2025-05-22 02:23:55 -0400

    The backEnd for medium mode is working, inshallah

--- app/input/page.tsx
+++ app/input/page.tsx
@@ -6,6 +6,7 @@ export default async function Page() {
 
     return (
         // blockID here is important. One needs to not fuck up with that
+        //Hello from the other side
         <div>
            <FillInTheBlanks blockID="91382eeb-f5ef-408f-a378-0f16c329f519" />
         </div>
