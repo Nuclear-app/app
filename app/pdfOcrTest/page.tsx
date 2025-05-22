154303e31f5106fe1aea5bb025b14975cbba4b5e
Sathya Sriram <98763491+sathyasriram777@users.noreply.github.com>
2025-05-21 23:31:53 -0400

    ocr

---
+++ app/pdfOcrTest/page.tsx
@@ -0,0 +1,11 @@
+import PdfOCR from "@/components/pdfOCR";
+
+const PdfOcrTest = () => {
+    return (
+        <div className="container mx-auto p-6">
+          <PdfOCR />
+        </div>
+      );
+}
+
+export default PdfOcrTest;
---
+++ lib/parseImage.ts
@@ -0,0 +1,23 @@
+import { createWorker } from 'tesseract.js';
+
+export const parseImage = async (image: File | null) => {
+    let ocrResult = '';
+    let ocrStatus = '';
+    if (!image) return;
+    const worker = await createWorker('eng', 1, {
+        logger: m => console.log(m),
+    });
+
+    try {
+        const { data: { text } } = await worker.recognize(image);
+        ocrResult = text;
+        ocrStatus = 'Success';
+    } catch (error) {
+        console.error('Error reading image:', error);
+        ocrStatus = 'Error';
+    } finally {
+        await worker.terminate();
+    }
+
+    return [ocrResult, ocrStatus];
+}
