630dc7c93bbad50c963e746b3dfbddbec1134d7c
Ojas S <ojas1807@gmail.com>
2025-07-12 16:05:39 -0400

    prisma update

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,6 +2,16 @@
 import { PrismaClient } from './generated/prisma'
 import { withAccelerate } from '@prisma/extension-accelerate'
 
-const prisma = new PrismaClient().$extends(withAccelerate())
+const globalForPrisma = globalThis as unknown as {
+  prisma: ReturnType<typeof createPrismaClient> | undefined
+}
+
+function createPrismaClient() {
+  return new PrismaClient().$extends(withAccelerate())
+}
+
+const prisma = globalForPrisma.prisma ?? createPrismaClient()
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
 export default prisma
